8d10611d3fcc9e1e0814bd6705329ada18d21c65
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-10-07 23:14:21 -0400

    heading

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,26 +1,26 @@
-import { Heading, Text, Box, Flex, Stack, Image, SimpleGrid} from '@chakra-ui/react';
+import { Heading, Text, Box, Flex, Stack, Image,Center, Square, SimpleGrid, Container} from '@chakra-ui/react';
 
 export default function IntroMe() {
 
 return (
 
-<SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
-<Box>
-<Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' />
-</Box>
-<Stack spacing={4}>
-  <Heading>Portfolio</Heading>
-  <Text color={'gray.500'} fontSize={'2xl'}>
+<SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+<Flex direction={'row'} color='white'>
+<Image
+  borderRadius='full'
+  boxSize='150px'
+  src='https://cdn.discordapp.com/attachments/985521628500877322/1028140475665887232/unknown.png'
+  alt='Dan Abramov'
+/>
+<Stack ml={'10'}>
+  <Heading fontSize={'5xl'}>Portfolio</Heading>
+  <Text color={'gray.500'} fontSize={'4xl'}>
     {/* this is where the wallet $$ api will go */}
     CA$154,000.90
   </Text>
 </Stack>
-<Flex>
-<Box>
-
-</Box>
-
 </Flex>
 </SimpleGrid>
+
  )
 }
