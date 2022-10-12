024720ceadc77da5fa9d4489b849468b763792f8
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-10-11 20:23:22 -0400

    tokens

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,7 +2,7 @@ import { Heading, Text, Box, Flex, Stack, Image,Center, Square, SimpleGrid, Divi
 import React, { useState, useEffect } from 'react'
 import HistoryCards from './WalletHistory';
 import Amount from './Amount';
-
+import TokenCard from './TokenHeld';
 export default function IntroMe() {
 return (
 <Container maxW={'100%'}>
@@ -19,12 +19,7 @@ return (
 </Flex>
 <Divider></Divider>
     <SimpleGrid mt='4'columns={2} spacing={'3'}>
-            <Box borderRadius='lg' borderWidth='3px' h={'425'} textAlign={'center'} maxW='100%'>
-                <Heading mb='2' mt='2'> 
-                  Wallet 
-                </Heading>
-                <Divider alignItems={'center'}/>
-              </Box>
+              <TokenCard />
               <HistoryCards />
     </SimpleGrid>
 </Container>
---
+++ components/TokenHeld.tsx
@@ -0,0 +1,52 @@
+import { Heading, Text, Box, Flex, Image,Center, Divider, HStack, VStack} from '@chakra-ui/react';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+export default function TokenCard() {
+
+  const url = 'https://api.covalenthq.com/v1/1/address/charles1.eth/balances_v2/?quote-currency=USD&format=JSON&nft=false&no-nft-fetch=false&key=ckey_a975e5abaecd4ae1911d109bc45'
+  const [data, setData] = useState(null) as any
+
+  useEffect(() => {
+      axios.get(url).then((response) => {
+          setData(response.data)
+      }).catch((error) => {
+          console.log(error)
+      })
+  }, [])
+
+
+  if (!data) return null
+
+return (
+        <Box borderRadius='lg' borderWidth='3px' textAlign={'center'} w='100%'>
+            <Heading mb='2' mt='2'> 
+              Tokens Held 
+            </Heading>
+            <Divider alignItems={'center'}/>
+              <Flex overflowY="auto" overflowX="auto" maxH={'359'} mt='1'>
+                <Center w='100%'>
+                    <VStack>
+                              <Box borderRadius='lg' borderWidth='3px' p={'3'} mt={'5'} w='100%'>
+                                <HStack>
+                                <Image
+                                    borderRadius='full'
+                                    boxSize='50px'
+                                    m='1'
+                                    src={data.data.items[1].logo_url}
+                                />
+                                <Text >
+                                    {data.data.items[1].contract_name}
+                                </Text>
+
+                                <Text>
+                                    {data.data.items[1].balance/1000000000000000000}                                
+                                </Text>
+                                </HStack>
+                              </Box>
+                      </VStack>
+                   </Center>
+                </Flex>
+        </Box>
+ )
+}
